d620cddb4b13a003ee58090a33b3d562dc272b78
teguhwinarno <teguhwinarno777@gmail.com>
2024-06-27 08:38:53 +0700

    penambahan program storing data ke sdcard

--- coba_sdcard/coba_sdcard.ino
+++ coba_sdcard/coba_sdcard.ino
@@ -1,91 +1,75 @@
-#include <Arduino.h>
 #include <SPI.h>
 #include <SD.h>
- #include <RH_RF95.h>
- #include <RHSoftwareSPI.h>
 
-// SD SPI pins can be chosen freely, but cannot overlap with other ports!
-#define SD_CS 13
-#define SD_SCK 14
-#define SD_MOSI 15
-#define SD_MISO 2
+// Definisikan pin untuk antarmuka SPI
+#define SD_CS   13   // CS pin untuk SD card module
+#define SD_MOSI 15   // MOSI pin untuk SD card module
+#define SD_SCK  14   // SCK pin untuk SD card module
+#define SD_MISO 2    // MISO pin untuk SD card module
 
-#define LORA_FREQ 868.0
-
-#define LOG_PATH "/lora_recv.log"
-
-// The sd card can also use a virtual SPI bus
-SPIClass sd_spi(HSPI);
-
-// Use a virtual (software) SPI bus for the sx1278
-RHSoftwareSPI sx1278_spi;
-RH_RF95 rf95(LORA_CS, LORA_IRQ, sx1278_spi);
-
+int counter = 0;
 void setup() {
-    // Builtin LED
-    pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial); // tunggu hingga serial port tersambung
 
-    // Serial output
-    Serial.begin(115200);
+  // Set up SPI interface
+  SPI.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
 
-    // LoRa: Init
-    pinMode(LORA_RST, OUTPUT);
-    digitalWrite(LORA_RST, LOW);
-    delay(100);
-    digitalWrite(LORA_RST, HIGH);
+  Serial.print("Initializing SD card...");
 
-    // the pins for the virtual SPI explicitly to the internal connection
-    sx1278_spi.setPins(LORA_MISO, LORA_MOSI, LORA_SCK);
+  if (!SD.begin(SD_CS)) {
+    Serial.println("Initialization of SD card failed!");
+    while (1);
+  }
+  Serial.println("SD card initialized.");
 
-    if (!rf95.init()) 
-        Serial.println("LoRa Radio: init failed.");
-    else
-        Serial.println("LoRa Radio: init OK!");
+  // Menulis data uji ke SD card
+  File dataFile = SD.open("/data.csv", FILE_WRITE);
+  if (dataFile) {
+    dataFile.println("SD card test");
+    dataFile.close();
+    Serial.println("Data written to SD card.");
+  } else {
+    Serial.println("Error opening test.txt");
+  }
 
-    // LoRa: set frequency
-    if (!rf95.setFrequency(LORA_FREQ))
-        Serial.println("LoRa Radio: setFrequency failed.");
-    else
-        Serial.printf("LoRa Radio: freqency set to %f MHz\n", LORA_FREQ);
-
-    rf95.setModemConfig(RH_RF95::Bw125Cr45Sf128);
-
-    // LoRa: Set max (23 dbm) transmission power. 
-    rf95.setTxPower(23, false);
-
-    // SD Card
-    sd_spi.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
-
-    if (!SD.begin(SD_CS, sd_spi))
-        Serial.println("SD Card: mounting failed.");
-    else 
-        Serial.println("SD Card: mounted.");
-
+  // Membaca data dari SD card
+  dataFile = SD.open("/data.csv");
+  if (dataFile) {
+    while (dataFile.available()) {
+      Serial.write(dataFile.read());
+    }
+    dataFile.close();
+    Serial.println("\nData read from SD card.");
+  } else {
+    Serial.println("Error opening test.txt");
+  }
 }
 
-uint8_t lora_buf[RH_RF95_MAX_MESSAGE_LEN];
-uint8_t lora_len;
-uint8_t receive_counter = 0;
-
 void loop() {
-    // Blink LED
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  counter++;
+  delay(1000);
+  // Menulis data uji ke SD card
+  File dataFile = SD.open("/data.csv", FILE_APPEND);
+  if (dataFile) {
+    dataFile.println(counter);
+    dataFile.close();
+    Serial.println("Data written to SD card.");
+  } else {
+    Serial.println("Error opening test.txt");
+  }
+  delay(1000);
 
-    lora_len = RH_RF95_MAX_MESSAGE_LEN;
-    if (rf95.recv(lora_buf, &lora_len)) {
-        receive_counter++;
-        Serial.printf("Received LoRa message #%i (%i bytes):\n%s\n", receive_counter, lora_len, lora_buf);
-
-        File test = SD.open(LOG_PATH, FILE_APPEND);
-        if (!test) {
-            Serial.println("SD Card: writing file failed.");
-        } else {
-            Serial.printf("SD Card: appending data to %s.\n", LOG_PATH);
-            test.write(lora_buf, lora_len);
-            test.printf("\n\n");
-            test.close();
-        }
+  // Membaca data dari SD card
+  dataFile = SD.open("/data.csv");
+  if (dataFile) {
+    while (dataFile.available()) {
+      Serial.write(dataFile.read());
     }
+    dataFile.close();
+    Serial.println("\nData read from SD card.");
+  } else {
+    Serial.println("Error opening test.txt");
+  }
 
-    delay(100);
 }
